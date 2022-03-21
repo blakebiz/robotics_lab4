b3bb8fb0fcf79fac6093baa15fd3d13b012ed2f6
blakebiz <blakebiz@github.com>
2022-03-21 16:03:37 +0000

    finished lab

--- scripts/detect_ball.py
+++ scripts/detect_ball.py
@@ -14,8 +14,10 @@ rgb_img = np.zeros((720, 1280, 3), dtype = "uint8")
 
 # topic /camera/color/image_raw
 def image_callback(ros_img):
+	# allow access to reassign global variables
 	global rgb_img
 	global img_received
+	# convert image to cv2 image and store it
 	rgb_img = CvBridge().imgmsg_to_cv2(ros_img, "rgb8")
 	img_received=True
 	
@@ -32,17 +34,29 @@ def filter_image(image):
 
 
 if __name__ == '__main__':
+	# initialize ros node
 	rospy.init_node('image_input', anonymous=True)
+	# declare publisher and subscriber
 	img_sub = rospy.Subscriber('/camera/color/image_raw', Image, image_callback)
 	img_pub = rospy.Publisher('/ball_2D', Image, queue_size=1)
 	
 	# set the frequency
 	rate = rospy.Rate(10)
+	
+	# declare array of 0's with the size of the image
+	rect = np.zeros((720, 1280), dtype="uint8")
+	# select a rectangle from the array
+	cv2.rectangle(rect, (500, 200), (700, 450), (255, 255, 255), -1)
 	while not rospy.is_shutdown():
 		# make sure we process if the camera has started streaming images
 		if img_received:
+			# filter original image to only contain yellows in a given range
 			altered_img = filter_image(rgb_img)
+			# logical and to get values only in the area we want
+			altered_img = cv2.bitwise_and(rect, altered_img)
+			# convert back to an image ros can use
 			img_msg = CvBridge().cv2_to_imgmsg(altered_img, encoding='mono8')
+			# publish image to /ball_2D for final output
 			img_pub.publish(img_msg)
 		rate.sleep()
 	
